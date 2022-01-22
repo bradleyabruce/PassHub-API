220b922dbeaeeefb8c681014f6b6f9efeb5da25e
Bradley Bruce <Bruceba@mail.uc.edu>
2022-01-21 21:46:56 -0700

    Initial PassHub changes

---
+++ PassHub-API/Controllers/PassHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace PassHub_API.Controllers
+{
+	[Route("PassHub")]
+	public class PassHub : Controller
+	{
+		[HttpGet]
+		// Add Validate Tag
+		[Route("GetAllPasswords")]
+		public ActionResult GetAllPasswords(string UserID)
+		{
+			var test = "hello";
+			return new EmptyResult();
+		}
+	}
+}
